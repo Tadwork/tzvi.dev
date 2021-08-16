96d2961e09a06a3373ff0c3d7ac691265a66a48f
Tzvi Friedman <tzvi.friedman@kustomer.com>
2021-08-15 21:17:00 -0400

    Use a different starter template

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,113 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
 module.exports = {
+  /* Your site config here */
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "tzvi.dev",
+    title: `Tzvi Friedman`,
+    author: `Tzvi Friedman`,
+    firstName: `Tzvi`,
+    lastName: `Friedman`,
+    description: `Tzvi Friedman's personal site`,
+    occupation: `Software Engineer`,
+    keywords: [
+      `Tzvi`,
+      `Friedman`,
+      `Personal`,
+      `Blog`,
+      `Resume`,
+      `Projects`,
+      `Work`,
+    ],
+    siteUrl:
+      process.env.URL || process.env.DEPLOY_URL || `http://localhost:8000`,
+    unemployed: false,
+    designations: [
+      `Web Developer`,
+      `Tech Lead`,
+      `Software Engineer`,
+      `Engineering Manager`,
+    ],
+    readingList: [
+      {
+        title: `Sapiens: A Brief History of Humankind`,
+        author: `Yuval Noah Harari`,
+        link: `https://www.goodreads.com/book/show/23692271-sapiens`,
+      },
+      {
+        title: `Behave: The Biology of Humans at our Best and Worst`,
+        author: `Robert Sapolsky`,
+        link: `https://www.goodreads.com/book/show/31170723-behave`,
+      },
+      {
+        title: `The Gene: An Intimate History`,
+        author: `Siddhartha Mukeherjee`,
+        link: `https://www.goodreads.com/book/show/27276428-the-gene`,
+      },
+    ],
+    showsList: [
+      {
+        title: `Avatar: The Last Airbender`,
+        author: `Micheal DiMartino, Bryan Konietzko`,
+        link: `https://www.imdb.com/title/tt0417299/`,
+      },
+      {
+        title: `Love, Death & Robots`,
+        author: `Tim Miller`,
+        link: `https://www.imdb.com/title/tt9561862/`,
+      },
+      {
+        title: `Star Wars: The Clone Wars`,
+        author: `George Lucas, Dave Filoni`,
+        link: `https://www.imdb.com/title/tt0458290/`,
+      },
+    ],
   },
   plugins: [
-    "gatsby-plugin-image",
+    `gatsby-plugin-preload-link-crossorigin`,
+    `gatsby-plugin-catch-links`,
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        trackingId: "UA-154644964-1",
+        name: "src",
+        path: `${__dirname}/src/`,
       },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: "src/images/icon.png",
+        name: `Tzvi Friedman's Personal Site`,
+        short_name: `J.Friedman`,
+        description: `This is my personal site.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `${__dirname}/static/favicon.ico`, // This path is relative to the root of the site.
       },
     },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: "gatsby-plugin-google-fonts",
       options: {
-        name: "images",
-        path: "./src/images/",
+        fonts: ["Raleway:300,400"],
+        display: "swap",
       },
-      __key: "images",
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-plugin-nprogress`,
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        color: `tomato`,
+        showSpinner: true,
       },
-      __key: "pages",
     },
   ],
-};
+}
